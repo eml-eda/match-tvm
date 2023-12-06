5eee35414160f6ba27fe71ef3eb42dd16138f2d1
Mohamed Amine Hamdi <mo.amine.hamdi@gmail.com>
2023-12-06 19:46:00 +0100

    feat: match api

--- python/tvm/relay/backend/contrib/match/codegen.py
+++ python/tvm/relay/backend/contrib/match/codegen.py
@@ -3,8 +3,9 @@ from . import _ffi_api
 @tvm._ffi.register_func("relay.ext.match")
 def byoc_compiler(mod: tvm.ir.IRModule):
     try:
-        import match
-        code=match.codegen.codegen(mod)
+        #breakpoint()
+        from match import codegen
+        code=codegen(mod)
         func_name = mod.attrs.global_symbol
         syms = [func_name]
         return _ffi_api.CSourceModuleCreate(code, "c", syms, [])
--- python/tvm/relay/op/contrib/match.py
+++ python/tvm/relay/op/contrib/match.py
@@ -1,6 +1,7 @@
 def partition_for_match(mod, params=None, dpu=None, **opts):
     try:
-        import match
-        return match.partition.partition(mod,params,dpu,opts)
+        #breakpoint()
+        from match import partition
+        return partition(mod,params,dpu,opts)
     except ImportError:
         raise ImportError("Match not detected correctly in your system!")
